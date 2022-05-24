c7443577c1ca53275db90810bcd010a242fba622
enzovasconcelos <enzo.vasconcelos@ccc.ufcg.edu.br>
2022-05-24 09:55:43 -0300

    Questão 1

--- RR4-01-Roteiro-Revisao-Recursao-environment/src/main/java/recursao/MetodosRecursivos.java
+++ RR4-01-Roteiro-Revisao-Recursao-environment/src/main/java/recursao/MetodosRecursivos.java
@@ -3,42 +3,31 @@ package recursao;
 public class MetodosRecursivos {
 
 	public int calcularSomaArray(int[] array){
-		int result = 0;
+		int result = 0;        
 		// TODO ESCREVA AQUI O CÓDIGO (USANDO RECURSAO) PARA CALCULAR A SOMA
 		// DOS EMENTOS DE UM ARRAY
 		return result;
 	}
+
 	public long calcularFatorial(int n) {
-		long result = 1;
-		// TODO ESCREVA AQUI O CÓDIGO (USANDO RECURSAO) PARA CALCULAR E IMPRIMIR
-		// O FATORIAL DO PARAMETRO
-		// DE ACORDO COM O QUE FOI MOSTRADO NO EXERCCICIO. OBSERVE QUE SENDO O
-		// METODO
-		// RECURSIVO, O FATORIAL DOS NUMEROS ANTERIORES TAMBEM VAO SER IMPRESSOS
-		return result;
+	    if(n == 0)
+            return 1;
+        long resultado = n * calcularFatorial(n - 1);
+        System.out.println(n + "! = " + resultado);
+        return resultado;
 	}
 
-	public int calcularFibonacci(int n) {
-		int result = 1;
-		// TODO ESCREVA AQUI O CÓDIGO (USANDO RECURSAO) PARA CALCULAR E IMPRIMIR
-		// O N-ESIMO TERMO
-		// DA SEQUENCIA DE FIBONACCI, QUE TEM A SEGUINTE LEI DE FORMACAO: O
-		// PRIMEIRO E SEGUNDO NUMEROS
-		// DA SEQUENCIA SÃO 1. A PARTIR DO TERCEIRO TERMO, CADA TERMO DA
-		// SEQUENCIA É DADO
-		// PELA SOMA DOS OUTROS DOIS ANTERIORES. OBSERVE QUE SENDO O METODO
-		// RECURSIVO, O FIBONACCI DOS NUMEROS ANTERIORES TAMBEM VAO SER
-		// IMPRESSOS
-		return result;
-	}
+    public int calcularFibonacci(int n) {
+        
+    }
 
-	public int countNotNull(Object[] array) {
-		int result = 0;
-		// TODO IMPLEMENTE AQUI O CODIGO QUE CONTA (USANDO RECURSAO) A
-		// QUANTIDADE DE ELEMENTOS NAO NULOS
-		// DE UM ARRAY DE OBJETOS RECEBIDO COMO PARAMETRO
-		return result;
-	}
+    public int countNotNull(Object[] array) {
+        int result = 0;
+        // TODO IMPLEMENTE AQUI O CODIGO QUE CONTA (USANDO RECURSAO) A
+        // QUANTIDADE DE ELEMENTOS NAO NULOS
+        // DE UM ARRAY DE OBJETOS RECEBIDO COMO PARAMETRO
+        return result;
+    }
 
 	public long potenciaDe2(int expoente) {
 		int result = 1;
