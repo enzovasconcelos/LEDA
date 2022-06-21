93a9637a4becfca55aa496e42212b3bd30a43b0a
enzovasconcelos <enzovasconcelos@ccc.ufcg.edu.br>
2022-06-21 09:52:49 -0300

    Adicionando R04-01

---
+++ R04-01-Rot-KLargestQuickSelectFloor-environment/src/main/java/problems/FloorBinarySearchImpl.java
@@ -0,0 +1,14 @@
+package problems;
+
+public class FloorBinarySearchImpl implements Floor {
+
+	@Override
+	public Integer floor(Integer[] array, Integer x) {
+	    Integer meio = busca(array, x, 0, array.length - 1);	
+	}
+
+    private Integer busca(Integer[] array, Interger numero, int left, int right) {
+      
+    }
+
+}
